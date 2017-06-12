debb58658d10fab93ea59cdbbd502ef28e1a4635
Peabody <peabody@dhcp-129-59-214-101.n1.vanderbilt.edu>
2017-06-12 11:40:43 -0500

    proj1

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -5,6 +5,21 @@
 # This program asks the user for his/her name and age.
 # Then, it prints a sentence that says when the user will turn 100.
 
-# If you complete extensions, describe your extensions here!
+#
+#
 
+name = raw_input("Enter a your name:")
+print name
+age= raw_input("Enter your age:")
+print age
+birthday = raw_input("Have you had a birthday already this year? yes or no:")
+print birthday
+if birthday == "yes":
+    print name + " will turn 100 in the year",
+    print 100 - int(age) + 2017
+if birthday == "no":
+    print name + " will turn 100 in the year",
+    print 100 - int(age) + 2016
+if age < "13":
+    print ""
 
