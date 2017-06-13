8ff736542d0d34bcdd07c5be0e56792779cd2ab3
Peabody <peabody@dhcp-129-59-214-101.n1.vanderbilt.edu>
2017-06-13 10:31:56 -0500

    proj4

--- proj04/proj04.py
+++ proj04/proj04.py
@@ -8,3 +8,24 @@ Asks the user for a string and prints out whether or not the string is a palindr
 
 """
 
+random_string = raw_input("Please enter a random word: ")
+paliList = []
+paliCheck = []
+original = []
+first_letter = random_string[0]
+rest = random_string[1:]
+first2 = first_letter.upper()
+random_string2 = first2 + rest
+random_string = random_string.replace(" ","")
+for symbol in random_string:
+    original.append(symbol)
+for letter in random_string:
+    paliList.append(letter)
+for item in paliList:
+    paliCheck.append(paliList[-1])
+    paliList = paliList[0:-1]
+print paliCheck
+if original == paliCheck:
+    print random_string2 + " is a palindrome."
+else:
+    print random_string2 + " is not a palindrome."
